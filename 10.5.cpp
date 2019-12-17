25460e086d84264a78dde760a58bbf3ab8e35dc7
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-17 14:54:26 +0300

    Create 10.5.cpp

---
+++ 10.5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int i,n;
+    float rez,a;
+    cout<<"Введите A и N"<<endl;
+    cin>>a>>n;
+    rez=1;
+    i=1;
+    while (i<=n)
+    {
+        rez=rez+(pow(a,i))*pow(-1,i);
+        i++;
+    }
+    cout<<"Результат="<<rez<<endl;
+    return 0;
+}
