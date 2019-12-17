3ab1523a7635866d694a52c98192730f771a2063
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-17 14:53:00 +0300

    Update 10.1.cpp

--- 10.1.cpp
+++ 10.1.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float cost;
+    cout<<"Введите цену конфет"<<endl;
+    cin>>cost;
+    for (int i=1; i<=10; i++)
+    {
+        cout<<"Стоимость="<<cost*0.1*i<<endl;
+    }
+    return 0;
+}
